8ae393fe86e10dae329adf89b2b94cba74a8afab
Ashraful Haque Tusher <bangladeshi.tushar@gmail.com>
2024-12-06 00:47:55 +0600

    customer file updated

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,22 @@
 // src/Customer.java
 public class Customer {
+    private static int idCounter = 1; // Static counter for generating unique IDs
     private String customerID;
     private String name;
 
-    public Customer(String customerID, String name) {
-        this.customerID = customerID;
+    public Customer(String name) {
+        this.customerID = "CUST00" + idCounter; // Generate ID with a prefix
+        idCounter++; // Increment the counter
         this.name = name;
     }
 
-    public String getCustomerID() { return customerID; }
-    public String getName() { return name; }
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
+
